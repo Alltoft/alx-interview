4f30e5972e16562b31658f48ffe095531175ea7d
Emran <ouakouriimran@gmail.com>
2024-06-06 21:23:43 +0100

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
+"""Interview Prep: Lockboxes"""
+
 
 Open = 'opened!'
 
 
 def OpenBox(boxes, n):
+    """
+    Recursively opens the boxes in a lockbox system.
+
+    Args:
+        boxes (list): A list of lists representing the lockbox system.
+        n (int): The index of the box to be opened.
+
+    Returns:
+        None
+    """
     box = boxes[n]
     if Open in box:
         return
@@ -17,6 +29,15 @@ def OpenBox(boxes, n):
 
 
 def canUnlockAll(boxes):
+    """
+    Determines if all the boxes in the given list can be unlocked.
+
+    Args:
+        boxes (list): A list of lists representing the lockboxes. Each inner list contains the keys to other boxes.
+
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
     OpenBox(boxes, n=0)
     for box in boxes:
         if Open in box:
