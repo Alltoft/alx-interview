ff7bf2387d2cc78fa5d37e510ad600c219dfd722
Emran <ouakouriimran@gmail.com>
2024-06-11 19:41:31 +0100

    0-minoperations.py

--- 0x02-minimum_operations/main.py
+++ 0x02-minimum_operations/main.py
@@ -4,7 +4,6 @@ Main file for testing
 """
 
 minOperations = __import__('0-minoperations').minOperations
-isprime = __import__('0-minoperations').isprime
 
 n = 4
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
