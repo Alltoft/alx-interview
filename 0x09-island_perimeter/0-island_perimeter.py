1b7798b44aceb2c8f89be685b97a3ea92a31bb03
Emran <ouakouriimran@gmail.com>
2024-08-01 14:24:26 +0100

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """A script that returns the perimeter of the island described in grid"""
 
+
 def island_perimeter(grid):
     """This function returns the perimeter of the island described in grid"""
     perim = 0
@@ -10,16 +11,12 @@ def island_perimeter(grid):
     for row in range(rows):
         for col in range(cols):
             if grid[row][col] == 1:
-                # Check top
                 if row == 0 or grid[row-1][col] == 0:
                     perim += 1
-                # Check left
                 if col == 0 or grid[row][col-1] == 0:
                     perim += 1
-                # Check right
                 if col == cols-1 or grid[row][col+1] == 0:
                     perim += 1
-                # Check bottom
                 if row == rows-1 or grid[row+1][col] == 0:
                     perim += 1
 
