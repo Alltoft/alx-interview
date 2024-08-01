395cfa0128bcb10dee778ddc798dc19ab813e0ce
Emran <ouakouriimran@gmail.com>
2024-08-01 14:19:35 +0100

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,30 +1,26 @@
 #!/usr/bin/python3
-"""a script that returns the perimeter of the island described in grid"""
-
+"""A script that returns the perimeter of the island described in grid"""
 
 def island_perimeter(grid):
-    """this function returns the perimeter of the island described in grid"""
-
+    """This function returns the perimeter of the island described in grid"""
     perim = 0
-    rown = 0
-    try:
-        for row in grid:
-            coln = 0
-            for column in row:
-                if column == 0:
-                    pass
-                else:
-                    if grid[rown-1][coln] == 0:
-                        perim += 1
-                    if grid[rown][coln-1] == 0:
-                        perim += 1
-                    if grid[rown][coln+1] == 0:
-                        perim += 1
-                    if grid[rown+1][coln] == 0:
-                        perim += 1
-                coln += 1
-            rown += 1
-    except IndexError:
-        Value = 0
+    rows = len(grid)
+    cols = len(grid[0]) if rows > 0 else 0
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                # Check top
+                if row == 0 or grid[row-1][col] == 0:
+                    perim += 1
+                # Check left
+                if col == 0 or grid[row][col-1] == 0:
+                    perim += 1
+                # Check right
+                if col == cols-1 or grid[row][col+1] == 0:
+                    perim += 1
+                # Check bottom
+                if row == rows-1 or grid[row+1][col] == 0:
+                    perim += 1
 
     return perim
